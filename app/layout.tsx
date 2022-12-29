456772a37f812601f53de62a4078fc7caafa322b
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-12-29 20:54:17 +0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
-import './globals.css'
+import "./globals.css";
+import Navbar from "./components/Navbar";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head />
-      <body>{children}</body>
+      <body className='px-6 md:px-16 bg-gray-900 2xl:max-w-[1280px] w-full'>
+        <div className=''>
+          <Navbar />
+        </div>
+        <div className='py-16 md:py-20 h-screen'>{children}</div>
+      </body>
     </html>
-  )
+  );
 }
