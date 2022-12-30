80fc185e7893bb8ada626f54adc7a63e525c4c30
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-12-30 19:28:30 +0300

    routing

---
+++ app/search/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBar from "../components/Search";
+
+export default function Search() {
+  return (
+    <div className=''>
+      <div className=''>
+        <SearchBar />
+      </div>
+    </div>
+  );
+}
