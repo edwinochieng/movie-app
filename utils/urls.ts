a1632b4bb7bb846528a18a2dc2f79a2cef2d0196
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-08 23:12:29 +0300

    urls

---
+++ utils/urls.ts
@@ -0,0 +1,5 @@
+export const baseURL = "https://api.themoviedb.org/3";
+
+export const posterURL = "https://image.tmdb.org/t/p/w342";
+
+export const backdropURL = '"https://image.tmdb.org/t/p/original"';
