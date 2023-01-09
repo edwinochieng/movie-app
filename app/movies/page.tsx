3338bfce17dd1ad97d9dd45c2961de40bb7d061f
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-09 17:04:11 +0300

    add movie sliders

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,5 +1,72 @@
 import React from "react";
+import Slider from "../components/Slider";
+import { baseURL } from "../../utils/urls";
 
-export default function MoviesPage() {
-  return <div>MoviesPage</div>;
+const getTrendingMovies = async () => {
+  const res = await fetch(
+    `${baseURL}/trending/movie/day?api_key=${process.env.API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched!");
+  }
+
+  return res.json();
+};
+
+const getPopularMovies = async () => {
+  const res = await fetch(
+    `${baseURL}/movie/popular?api_key=${process.env.API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched!");
+  }
+
+  return res.json();
+};
+const getTopRatedMovies = async () => {
+  const res = await fetch(
+    `${baseURL}/movie/top_rated?api_key=${process.env.API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched!");
+  }
+
+  return res.json();
+};
+
+const getNowPlaying = async () => {
+  const res = await fetch(
+    `${baseURL}/movie/now_playing?api_key=${process.env.API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched!");
+  }
+
+  return res.json();
+};
+export default async function MoviesPage() {
+  const trendingMovies = await getTrendingMovies();
+  const popularMovies = await getPopularMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const nowPlaying = await getNowPlaying();
+
+  const [trending, popular, topRated, inTheatres] = await Promise.all([
+    trendingMovies,
+    popularMovies,
+    topRatedMovies,
+    nowPlaying,
+  ]);
+
+  return (
+    <div>
+      <Slider title='Trending Now' sliderID='1' data={trending} />
+      <Slider title='Popular' sliderID='2' data={popular} />
+      <Slider title='Top Rated' sliderID='3' data={topRated} />
+      <Slider title='In Theatres' sliderID='4' data={inTheatres} />
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
+import Slider from "./components/Slider";
+import { baseURL } from "../utils/urls";
 
-export default function Home() {
+const getTrending = async () => {
+  const res = await fetch(
+    `${baseURL}/trending/all/day?api_key=${process.env.API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched!");
+  }
+
+  return res.json();
+};
+export default async function Home() {
+  const data = await getTrending();
+  console.log(data);
+
   return (
-    <div>Hello world!</div>
-  )
+    <div className='font-bold text-white'>
+      <Slider title='Trending Now' sliderID='1' data={data} />
+    </div>
+  );
 }
