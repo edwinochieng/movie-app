3dfa7ee16af91c1c71cb165919e02ef50f85c11b
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-11 22:52:51 +0300

    reduce padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ export default function RootLayout({
   return (
     <html lang='en' className={quicksand.className}>
       <head />
-      <body className='px-6 md:px-16 bg-gray-900 w-full'>
+      <body className='px-4 md:px-6 lg:px-16 bg-gray-900 w-full'>
         <div className=''>
           <Navbar />
         </div>
-        <div className='py-16 md:py-20 h-screen'>{children}</div>
+        <div className='py-12 md:py-16 h-screen'>{children}</div>
       </body>
     </html>
   );
