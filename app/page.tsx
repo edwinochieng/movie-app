9e0625f04a1d1a1945cff765c1417010a90e6379
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-11 22:53:28 +0300

    add Banner + Movie and Tv sliders

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Slider from "./components/Slider";
 import { baseURL } from "../utils/urls";
+import Banner from "./components/Banner";
 
 const getTrending = async () => {
   const res = await fetch(
@@ -12,13 +13,52 @@ const getTrending = async () => {
 
   return res.json();
 };
-export default async function Home() {
-  const data = await getTrending();
-  console.log(data);
 
+const getPopularMovies = async () => {
+  const res = await fetch(
+    `${baseURL}/movie/popular?api_key=${process.env.API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched!");
+  }
+
+  return res.json();
+};
+
+const getPopularShows = async () => {
+  const res = await fetch(
+    `${baseURL}/tv/popular?api_key=${process.env.API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched!");
+  }
+
+  return res.json();
+};
+
+export default async function Home() {
+  const trendingShows = await getTrending();
+  const popularShows = await getPopularShows();
+  const popularMovies = await getPopularMovies();
+
+  const [trending, shows, movies] = await Promise.all([
+    trendingShows,
+    popularShows,
+    popularMovies,
+  ]);
   return (
     <div className='font-bold text-white'>
-      <Slider title='Trending Now' sliderID='1' data={data} />
+      <div className='z-[-10]'>
+        <Banner data={trending} />
+      </div>
+
+      <div className=''>
+        <Slider title='Trending Now' sliderID='1' data={trending} />
+        <Slider title='TV Shows' sliderID='2' data={shows} />
+        <Slider title='Movies' sliderID='3' data={movies} />
+      </div>
     </div>
   );
 }
