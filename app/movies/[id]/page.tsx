db80292d701a682623ffc15d4dbd31e4e211408d
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-12 23:16:14 +0300

    movie details api calls

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,5 +1,48 @@
 import React from "react";
 
-export default function MovieDetails() {
-  return <div>MovieDetails</div>;
+import { baseURL } from "../../../utils/urls";
+import Details from "../../components/Details";
+
+const getMovieDetails = async (id: number) => {
+  const res = await fetch(
+    `${baseURL}/movie/${id}?api_key=${process.env.API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched!");
+  }
+
+  return res.json();
+};
+
+const getMovieRecommendations = async (id: number) => {
+  const res = await fetch(
+    `${baseURL}/movie/${id}/recommendations?api_key=${process.env.API_KEY}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched!");
+  }
+
+  return res.json();
+};
+
+export default async function MovieDetails({
+  params,
+}: {
+  params: { id: number };
+}) {
+  const movieDetails = await getMovieDetails(params.id);
+  const movieRecommendations = await getMovieRecommendations(params.id);
+
+  const [details, recommendations] = await Promise.all([
+    movieDetails,
+    movieRecommendations,
+  ]);
+
+  return (
+    <div>
+      <Details data={details} />
+    </div>
+  );
 }
