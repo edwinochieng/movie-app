868f5e52fdc2a757b7f1cc2d8b8b09dc2afe1199
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-15 23:30:43 +0300

    rewrite Searchbar code

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,82 +1,34 @@
 "use client";
-import { Fragment, useState } from "react";
-import { Combobox, Transition } from "@headlessui/react";
-import { MdOutlineSearch, MdOutlineClear } from "react-icons/md";
-
-const people = [
-  { id: 1, name: "Wade Cooper" },
-  { id: 2, name: "Arlene Mccoy" },
-  { id: 3, name: "Devon Webb" },
-  { id: 4, name: "Tom Cook" },
-  { id: 5, name: "Tanya Fox" },
-  { id: 6, name: "Hellen Schmidt" },
-];
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { MdOutlineSearch } from "react-icons/md";
 
 export default function SearchBar() {
   const [query, setQuery] = useState<string>("");
+  const router = useRouter();
 
-  const filteredPeople =
-    query === ""
-      ? people
-      : people.filter((person) =>
-          person.name
-            .toLowerCase()
-            .replace(/\s+/g, "")
-            .includes(query.toLowerCase().replace(/\s+/g, ""))
-        );
+  const handleSubmit = (e: any): void => {
+    e.preventDefault();
+    router.push(`/search/${query}`);
+  };
 
   return (
     <div className='relative max-w-[560px] mx-auto w-full'>
-      <Combobox>
-        <div className='relative mt-1'>
-          <div className='relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm'>
-            <Combobox.Input
-              className='w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0'
-              value={query}
-              onChange={(event) => setQuery(event.target.value)}
-            />
-            <Combobox.Button className='absolute inset-y-0 right-0 flex items-center pr-2'>
-              {query ? (
-                <MdOutlineClear size={18} />
-              ) : (
-                <MdOutlineSearch
-                  className='h-5 w-5 text-gray-400'
-                  aria-hidden='true'
-                />
-              )}
-            </Combobox.Button>
-          </div>
-          <Transition
-            as={Fragment}
-            leave='transition ease-in duration-100'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
-            afterLeave={() => setQuery("")}
-          >
-            <Combobox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
-              {filteredPeople.length === 0 && query !== "" ? (
-                <div className='relative cursor-default select-none py-2 px-4 text-gray-700'>
-                  Nothing found.
-                </div>
-              ) : (
-                filteredPeople.map((person) => (
-                  <Combobox.Option
-                    key={person.id}
-                    className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? "bg-cyan-800 text-white" : "text-gray-900"
-                      }`
-                    }
-                    value={person}
-                  >
-                    <span className={`block truncate`}>{person.name}</span>
-                  </Combobox.Option>
-                ))
-              )}
-            </Combobox.Options>
-          </Transition>
+      <form
+        onSubmit={handleSubmit}
+        className='relative flex items-center w-full h-10 shadow-2xl shadow-gray-500 rounded-lg focus-within:shadow-xl bg-white overflow-hidden'
+      >
+        <input
+          className='h-full w-full outline-none text-[14px] font-medium text-gray-800 pl-3 placeholder:text-gray-400'
+          type='text'
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          placeholder='Search Movie, Tv Show or Actors..'
+        />
+        <div className='grid place-items-center h-full w-12 text-gray-500 cursor-pointer'>
+          <MdOutlineSearch onClick={handleSubmit} size={20} />
         </div>
-      </Combobox>
+      </form>
     </div>
   );
 }
