c1643637924eddd035c5f8e8ca171efd85ccbe96
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-15 23:49:12 +0300

    fetch searched query

--- app/search/[slug]/page.tsx
+++ app/search/[slug]/page.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { baseURL } from "../../../utils/urls";
 
-export default function SearchResults() {
+const getSearched = async (query: string) => {
+  const res = await fetch(
+    `${baseURL}/search/multi?api_key=${process.env.API_KEY}&&query=${query}`,
+    { cache: "no-store" }
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched!");
+  }
+
+  return res.json();
+};
+
+export default async function SearchResults({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data = await getSearched(params.slug);
+
   return <div>SearchResults</div>;
 }
