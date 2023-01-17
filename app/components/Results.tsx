4d922b72cd1080136f3b0ab9343124d847bdd858
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-17 16:35:37 +0300

    search results display card

--- app/components/Results.tsx
+++ app/components/Results.tsx
@@ -6,20 +6,21 @@ import React from "react";
 import { Results } from "../../utils/interfaces";
 import { posterURL } from "../../utils/urls";
 
-interface Movie {
-  movie: Results;
+interface SearchResults {
+  data: Results;
 }
 
 export default function SearchedResults({
-  movie: {
+  data: {
     id,
     poster_path,
     first_air_date,
     original_name,
     release_date,
     title,
+    profile_path,
   },
-}: Movie) {
+}: SearchResults) {
   const router = useRouter();
 
   const handleClick = (): void => {
@@ -27,15 +28,18 @@ export default function SearchedResults({
       router.push(`/movies/${id}`);
     } else if (first_air_date || original_name) {
       router.push(`/tvshows/${id}`);
+    } else if (profile_path) {
+      router.push(`/actors/${id}`);
     }
   };
+
   return (
     <div
       className='w-[155px] md:w-[175px] cursor-pointer'
       onClick={handleClick}
     >
       <Image
-        src={`${posterURL}${poster_path}`}
+        src={`${posterURL}${poster_path || profile_path}`}
         height='300'
         width='300'
         alt={original_name || title}
