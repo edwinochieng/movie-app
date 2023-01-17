a6f5af17676bb52781157d549a6fc68a7a4c2416
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-17 21:24:53 +0300

    add alternative image url

--- utils/urls.ts
+++ utils/urls.ts
@@ -3,3 +3,5 @@ export const baseURL = "https://api.themoviedb.org/3";
 export const posterURL = "https://image.tmdb.org/t/p/w500";
 
 export const backdropURL = "https://image.tmdb.org/t/p/original";
+
+export const alternativeImage = "https://media.cheggcdn.com/media/23d/23d30c4d-6bc4-482f-9da1-8493bc4af800/phpMojsnG"
