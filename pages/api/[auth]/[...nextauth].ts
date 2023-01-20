5e585023aaee782cbbbb5d79ed5a3898d2e71f27
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-20 21:15:45 +0300

    set up nextauth

---
+++ pages/api/[auth]/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth/next";
+
+export default NextAuth({
+    session:{
+        strategy:'jwt',
+    },
+    callbacks:{
+        async jwt({token,user}){
+            if(user?._id) token._id = user._id
+            return token
+        }
+        async session({session,token}){
+            if(token?._id) session.user._id = token._id
+        }
+    }
+})
