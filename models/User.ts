74cc819b512d4bd267af16bce0d21dde61e57cfb
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-01-22 00:02:06 +0300

    add password field to userSchema

--- models/User.ts
+++ models/User.ts
@@ -3,12 +3,14 @@ import mongoose from "mongoose";
 interface User {
     name:string
     email:string
+    password:string 
 }
 
 const userSchema = new mongoose.Schema<User>({
 
         name:{ type: String, required:true},
         email :{type: String, required:true, unique: true},
+        password: {type:String, required:true }
     
 })
 
