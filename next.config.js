85332a6fdeda512613d170d71e5a6635e1a61baa
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-07-13 00:32:31 +0300

    remove experimental appDir configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ["image.tmdb.org","media.cheggcdn.com"],
+    domains: ["image.tmdb.org", "media.cheggcdn.com"],
   },
 };
